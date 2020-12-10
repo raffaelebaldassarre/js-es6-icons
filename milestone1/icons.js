6730c4911b67dc17a5a5b28f2fdbf3c563ee1175
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2020-12-10 17:36:04 +0100

    milestone1

--- milestone1/icons.js
+++ milestone1/icons.js
@@ -1,9 +1,54 @@
 $(document).ready(function () {
-  // Creiamo array di oggetti per le icone con le seguenti proprieta: name, prefix, type, family 
+  // Creiamo array di oggetti per le icone con le seguenti proprieta: name, prefix, type, family
+  
+  class Icon {
+    constructor(name, prefix, type, family){
+      this.name = name;
+      this.prefix = prefix;
+      this.type = type;
+      this.family = family
+    }
+  }
 
-  // Semezioniamo il container icons
+  const icons = [
+    new Icon("cat", "fas", "fa-cat", "animals"),
+    new Icon("crow", "fas", "fa-crow", "animals"),
+    new Icon("dog", "fas", "fa-dog", "animals"),
+    new Icon("dove", "fas", "fa-dove", "animals"),
+    new Icon("dragon", "fas", "fa-dragon", "animals"),
+    new Icon("horse", "fas", "fa-horse", "animals"),
+    new Icon("hippo", "fas", "fa-hippo", "animals"),
+    new Icon("fish", "fas", "fa-fish", "animals"),
+    new Icon("carrot", "fas", "fa-carrot", "fruits & vegetables"),
+    new Icon("apple-alt", "fas", "fa-apple-alt", "fruits & vegetables"),
+    new Icon("lemon", "fas", "fa-lemon", "fruits & vegetables"),
+    new Icon("pepper-hot", "fas", "fa-pepper-hot", "fruits & vegetables"),
+    new Icon("user-astronaut","fas", "fa-user-astronaut","users & people"),
+    new Icon("user-graduate","fas", "fa-user-graduate","users & people"),
+    new Icon("user-ninja","fas", "fa-user-ninja","users & people"),
+    new Icon("user-secret","fas", "fa-user-secret","users & people"),
+
+  ];
+  console.log(icons);
+
+  // Selezioniamo il container icons
+
+  const container = document.getElementById("icons");
 
   //inseriamo le icone nel container (possiamo creare una funzione tipo print() per inserire gli elementi e richiamarla qui)
+  icons.forEach(icon => {
+    const markup = `
+    <div>
+       <i class="${icon.prefix} ${icon.type}"></i>
+       <div class="title">
+       ${icon.name.toUpperCase()}
+       </div>
+    </div>
+    `;
+    container.insertAdjacentHTML('beforeend', markup);
+
+});
+
 
 });
 
@@ -11,5 +56,4 @@ $(document).ready(function () {
 /* ---- FUNCTIONS ----*/
 // Creiamo una funzione per iterare nell'array e che appenda al container le icone.
 // tip: possiamo usare un ciclo foreach qui e destrutturare gli le proprieta degli elementi di un array
-// tip: il template literal ci puo aiutare con il markup
-
+//tip: il template literal ci puo aiutare con il markup
