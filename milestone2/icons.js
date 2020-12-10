833e29cf51eaaedc3b77661f47d4d8c15c0919a6
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2020-12-10 17:55:27 +0100

    milestone2.. work in progress

--- milestone2/icons.js
+++ milestone2/icons.js
@@ -1,13 +1,56 @@
 $(document).ready(function () {
   // Qui le icone che abbiamo definito nella milestone 1
+  class Icon {
+    constructor(name, prefix, type, family){
+      this.name = name;
+      this.prefix = prefix;
+      this.type = type;
+      this.family = family
+    }
+  }
+
+  const icons = [
+    new Icon("cat", "fas", "fa-cat", "animals"),
+    new Icon("crow", "fas", "fa-crow", "animals"),
+    new Icon("dog", "fas", "fa-dog", "animals"),
+    new Icon("dove", "fas", "fa-dove", "animals"),
+    new Icon("dragon", "fas", "fa-dragon", "animals"),
+    new Icon("horse", "fas", "fa-horse", "animals"),
+    new Icon("hippo", "fas", "fa-hippo", "animals"),
+    new Icon("fish", "fas", "fa-fish", "animals"),
+    new Icon("carrot", "fas", "fa-carrot", "fruits & vegetables"),
+    new Icon("apple-alt", "fas", "fa-apple-alt", "fruits & vegetables"),
+    new Icon("lemon", "fas", "fa-lemon", "fruits & vegetables"),
+    new Icon("pepper-hot", "fas", "fa-pepper-hot", "fruits & vegetables"),
+    new Icon("user-astronaut","fas", "fa-user-astronaut","users & people"),
+    new Icon("user-graduate","fas", "fa-user-graduate","users & people"),
+    new Icon("user-ninja","fas", "fa-user-ninja","users & people"),
+    new Icon("user-secret","fas", "fa-user-secret","users & people"),
+
+  ];
 
   // definiamo dei colori per le icone (blue, orange, purple)
-
+  const blue = "#0000FF";
+  const orange = "#FFA502";
+  const purple = "#800080";
 
   //aggiungiamo dei colori usando una funzione
 
 
   //inseriamo le icone colorate nel container
+  const container = document.getElementById("icons");
+  icons.forEach(icon => {
+    const markup = `
+    <div>
+       <i class="${icon.prefix} ${icon.type}"></i>
+       <div class="title">
+       ${icon.name.toUpperCase()}
+       </div>
+    </div>
+    `;
+    container.insertAdjacentHTML('beforeend', markup);
+
+  });
 
 });
 
